7eea2776dd1cc1970dc7168a71f1fdeb2398e52b
Jujin <jujin@jujinkim.com>
2020-11-01 21:55:19 +0900

    initial commit

---
+++ app/src/main/java/com/jujinkim/dagger2chef/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jujinkim.dagger2chef
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
