64f2fd3b4417ea8a1ee1dacca11a90352cbf3b67
Jujin <jujin@jujinkim.com>
2020-11-02 23:28:17 +0900

    Add 'eat()' method at Ramen.kt and call it from MainActivity.kt

--- app/src/main/java/com/jujinkim/dagger2chef/MainActivity.kt
+++ app/src/main/java/com/jujinkim/dagger2chef/MainActivity.kt
@@ -2,10 +2,17 @@ package com.jujinkim.dagger2chef
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jujinkim.dagger2chef.market.Market
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val market = Market()
+        val chef = Chef(market = market)
+
+        val ramen = chef.cookRamen()
+        ramen.eat()
     }
 }
--- app/src/main/java/com/jujinkim/dagger2chef/model/Ramen.kt
+++ app/src/main/java/com/jujinkim/dagger2chef/model/Ramen.kt
@@ -1,3 +1,5 @@
 package com.jujinkim.dagger2chef.model
 
-class Ramen(water: Water, sauce: Sauce, noodle: Noodle)
+class Ramen(water: Water, sauce: Sauce, noodle: Noodle) {
+    fun eat() = println("yum")
+}
